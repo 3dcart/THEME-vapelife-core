d0bd0cf8a094166e2e8f5f4afeab995625d76b03
asaghir <asaghir@3dcart.com>
2023-04-25 16:47:35 -0400

    DFN-1989 - CLS issues

--- VapeLife-core/js/main.js
+++ VapeLife-core/js/main.js
@@ -179,5 +179,5 @@ jQuery(function ($) {
 
 });
 setTimeout(function(){
-      $('.antiCls').css("visibility", "visible");
+      $('.antiCls, .antiCls-xs').css("visibility", "visible");
   }, 800);
